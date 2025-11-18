aaa7aa53a4289625da899da7f8bf4f2477a1a0f7
Vu The Vinh <156775187+vuthevinh2125@users.noreply.github.com>
2025-11-18 15:01:49 +0700

    Delete UrlShortener.Api/Models directory

--- UrlShortener.Api/Models/ShortenedUrl.cs
+++
@@ -1,9 +0,0 @@
-// UrlShortener.Api/Models/ShortenedUrl.cs
-
-public class ShortenedUrl
-{
-    public int Id { get; set; } // Primary Key
-    public string LongUrl { get; set; } = string.Empty;
-    public string ShortCode { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; }
-}
